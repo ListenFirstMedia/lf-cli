9481a2a29d0aec65b551002f84176a24f41108b3
Mike Stanley <mike.stanley@listenfirstmedia.com>
2020-07-22 22:11:19 -0400

    implemented configuration verification

--- src/commands/config/verify.ts
+++ src/commands/config/verify.ts
@@ -1,16 +1,52 @@
-import { Command, flags } from '@oclif/command';
-import { profileFlag } from '../../cli-flags';
+import { flags } from '@oclif/command';
+import BaseCommand from '../../base-command';
+import * as Listr from 'listr';
+import { obtainAccessToken } from '../../lfapi/auth';
+import Client from '../../lfapi/client';
 
-export default class ConfigVerify extends Command {
+export default class ConfigVerify extends BaseCommand {
     static description = 'Verify the profile';
 
     static flags = {
         help: flags.help({ char: 'h' }),
-        profile: profileFlag(),
+        ...BaseCommand.flags,
     };
 
     async run() {
-        const opts = this.parse(ConfigVerify);
-        this.log(`verifying ${opts.flags.profile}`);
+        this.log(`Verifying configuration profile`);
+
+        const tasks = new Listr([
+            {
+                title: 'Loading profile settings',
+                task: async (ctx) => {
+                    const profile = await this.lfapiConfigProfile();
+                    ctx.profile = profile;
+                    return profile;
+                },
+            },
+            {
+                title: 'Obtaining access token',
+                enabled: (ctx) => ctx.profile !== undefined,
+                task: async (ctx) => {
+                    const token = await obtainAccessToken(ctx.profile);
+                    ctx.token = token;
+                    return token;
+                },
+            },
+            {
+                title: 'Making a secure request',
+                enabled: (ctx) => ctx.token !== undefined,
+                task: async (ctx) => {
+                    const client = new Client(ctx.token, ctx.profile);
+                    const res = await client.fetch('/v20200626/brand_views');
+                    if (res && res.records && res.records.length > 0) {
+                        return true;
+                    }
+                    throw new Error('Failed to make a secure request');
+                },
+            },
+        ]);
+
+        await tasks.run();
     }
 }
