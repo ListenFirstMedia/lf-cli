046cdc7970d074f754a1b76c19934b64194b1b54
Mike Stanley <mike.stanley@listenfirstmedia.com>
2020-07-23 15:21:26 -0400

    implemented datasets index

--- src/commands/datasets/index.ts
+++ src/commands/datasets/index.ts
@@ -1,27 +1,72 @@
-import { Command, flags } from '@oclif/command';
+import { flags } from '@oclif/command';
+import BaseCommand from '../../base-command';
+import cli from 'cli-ux';
+import * as _ from 'lodash';
 
-export default class DatasetsIndex extends Command {
-    static description = 'describe the command here';
+export default class DatasetsIndex extends BaseCommand {
+    static description = 'List available datasets';
 
     static flags = {
-        help: flags.help({ char: 'h' }),
-        // flag with a value (-n, --name=VALUE)
-        name: flags.string({ char: 'n', description: 'name to print' }),
-        // flag with no value (-f, --force)
-        force: flags.boolean({ char: 'f' }),
+        format: flags.string({
+            description: 'output format of the results',
+            default: 'raw',
+            options: ['raw', 'table', 'doc'],
+        }),
+        pretty: flags.boolean({
+            description:
+                'pretty print json responses (applies to raw or doc formats)',
+            default: false,
+        }),
+        ...BaseCommand.flags,
+        ...cli.table.flags({ except: ['extended', 'output'] }),
     };
 
-    static args = [{ name: 'file' }];
-
     async run() {
-        const { args, flags } = this.parse(DatasetsIndex);
-
-        const name = flags.name ?? 'world';
-        this.log(
-            `hello ${name} from /Users/mps/git/lf-cli/src/commands/datasets/index.ts`
-        );
-        if (args.file && flags.force) {
-            this.log(`you input --force and --file: ${args.file}`);
+        const opts = this.parse(DatasetsIndex);
+        const client = await this.lfmapClient();
+        cli.action.start('fetching datasets');
+        const res = await client.fetch('/v20200626/dictionary/datasets');
+        cli.action.stop();
+        switch (opts.flags.format) {
+            case 'raw':
+                if (opts.flags.pretty) {
+                    this.pp(res);
+                } else {
+                    this.log(JSON.stringify(res));
+                }
+                break;
+            case 'doc':
+                if (opts.flags.pretty) {
+                    res.records.forEach((rec: any) => this.pp(rec));
+                } else {
+                    res.records.forEach((rec: any) =>
+                        this.log(JSON.stringify(rec))
+                    );
+                }
+                break;
+            case 'table':
+                cli.table(
+                    res.records,
+                    {
+                        id: {
+                            header: 'ID',
+                        },
+                        name: {
+                            minWidth: 7,
+                        },
+                        analysis_type: {},
+                        dataset_type: {},
+                        description: {},
+                    },
+                    {
+                        printLine: this.log,
+                        ..._.pick(opts.flags, _.keys(cli.table.flags())),
+                    }
+                );
+                break;
+            default:
+                this.error('Unexpected output format');
+                this.exit(1);
         }
     }
 }
