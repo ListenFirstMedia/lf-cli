5db6e6675278b148c5b6be1b415a5a0fd53288d8
Khrisendat Persaud <khris.persaud@listenfirstmedia.com>
2020-12-15 17:28:24 +0000

    fix typo

--- src/commands/platform/data-status.ts
+++ src/commands/platform/data-status.ts
@@ -3,11 +3,11 @@ import ApiCommand from '../../api-command';
 export default class DataStatus extends ApiCommand {
     static description = `Get data status`;
 
-    static examples = ['$ lf-cli platform:data_status'];
+    static examples = ['$ lf-cli platform:data-status'];
 
     async run() {
         const path = `/v20200626/platform/data_status`;
-        const res = await this.fetch(path, undefined, `fetching data_status'`);
+        const res = await this.fetch(path, undefined, `fetching data_status`);
         const apiFlags = this.parsedApiFlags();
         if (apiFlags.pretty) {
             this.pp(res);
