ddf6d24e8da78c8c6b4ba032309635824067e0e4
Gregory Travis <greg.travis@listenfirstmedia.com>
2021-11-23 14:16:33 -0500

    send post as json

--- src/commands/bulk-tag-ingest/get.ts
+++ src/commands/bulk-tag-ingest/get.ts
@@ -1,4 +1,7 @@
+import * as querystring from 'querystring';
+import * as fs from 'fs';
 import ApiCommand from '../../api-command';
+import * as csv from "csvtojson";
 
 export default class BulkTagIngestGet extends ApiCommand {
     static description = `Ingest tags`;
@@ -18,6 +21,7 @@ export default class BulkTagIngestGet extends ApiCommand {
     static examples = ['$ lf-cli bulk-tag-ingest:get tags.csv'];
 
     async run() {
+    /* GET with filename
         const opts = this.parse(BulkTagIngestGet);
 
         if (opts.args.ID === 'help') {
@@ -31,6 +35,59 @@ export default class BulkTagIngestGet extends ApiCommand {
             `/v20200626/tag?job_id=5678&filename=${filename}`,
             undefined,
             `Ingesting tags from ${filename}'`
+        );
+
+        this.outputRecords(res);
+        */
+
+        const opts = this.parse(BulkTagIngestGet);
+
+        if (opts.args.ID === 'help') {
+            await BulkTagIngestGet.run(['-h']);
+            this.exit(0);
+        }
+
+        //const csv_contents = fs.readFileSync('tags.csv','utf8');
+        //console.log('tags.csv contents', csv_contents);
+        const jsonified = await csv(/*{
+            output: "csv"
+            }*/)
+        //.fromString(csv_contents)
+        .fromFile('tags.csv')
+        .then(function(result){
+          console.log('csv result', result);
+          //const jsonified = result;
+          console.log('jsonified', result);
+          return result;
+        });
+        //console.flush();
+        //process.exit();
+
+        const authData = JSON.stringify(jsonified);
+
+        /*
+        const authData = JSON.stringify({
+        job_id: 5678,
+        filename: 'input.csv'
+        });
+        */
+        console.log('authData json', authData);
+        //const authData = "5678,input.csv\n";
+
+        // prepare the POST request
+        const authReqOpts = {
+            method: 'POST',
+            body: authData,
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'content-length': Buffer.byteLength(authData).toString(),
+            },
+        };
+
+        const res = await this.fetch(
+            `/v20200626/tag`,
+            authReqOpts,
+            `Ingesting tags`
         );
 
         this.outputRecords(res);
