d9b1e0009d1e731b9efcc35d75c4c88e7d079771
Gregory Travis <greg.travis@listenfirstmedia.com>
2021-11-23 11:10:34 -0500

    cleanup

--- src/commands/bulk-tag-ingest/get.ts
+++ src/commands/bulk-tag-ingest/get.ts
@@ -19,75 +19,20 @@ export default class BulkTagIngestGet extends ApiCommand {
 
     async run() {
         const opts = this.parse(BulkTagIngestGet);
-        console.log('hey');
-        console.log(opts.args.filename);
-
-        // const fopts = { 'job_id': 5678, 'filename': 'input.csv' };
-        // const response = await fetch('https://httpbin.org/post', {method: 'POST', body: 'a=1'});
-        const params = new URLSearchParams();
-        params.append('job_id', 5678);
-        params.append('filename', 'input.csv');
-
-        const fopts = { method: 'POST', body: JSON.stringify(params) };
-
-        //const response = await fetch('https://httpbin.org/post', {method: 'POST', body: params});
-        //const data = await response.json();
-
-        /*
-        const res3 = await this.fetch(
-            `/v20200626/tag`,
-            fopts, // undefined,
-            `fetching Brand View Set 5678'`
-        );
-        */
-        const res3 = await this.fetch(
-            `/v20200626/tag?job_id=5678&filename=input.csv`,
-            undefined,
-            `fetching Brand View Set 5678'`
-        );
-
-        const cols2 = {
-            id: {
-                header: 'ID',
-                minWidth: 10,
-            },
-            name: {},
-        };
-
-        this.outputRecords(res3, cols2);
-
-        // process.exit();
-        return;
-
-        const res2 = await this.fetch(
-            `/v20200626/brand_view_sets/${opts.args.ID}`,
-            undefined,
-            `fetching Brand View Set ${opts.args.ID}'`
-        );
 
         if (opts.args.ID === 'help') {
-            await BrandViewSetGet.run(['-h']);
+            await BulkTagIngestGet.run(['-h']);
             this.exit(0);
         }
 
-        if (!opts.args.ID.match(/^[\d]+$/i)) {
-            this.error('Invalid Brand View Set ID', { exit: 1 });
-        }
+        const filename = opts.args.filename;
 
         const res = await this.fetch(
-            `/v20200626/brand_view_sets/${opts.args.ID}`,
+            `/v20200626/tag?job_id=5678&filename=${filename}`,
             undefined,
-            `fetching Brand View Set ${opts.args.ID}'`
+            `Ingesting tags from ${filename}'`
         );
 
-        const cols = {
-            id: {
-                header: 'ID',
-                minWidth: 10,
-            },
-            name: {},
-        };
-
-        this.outputRecords(res, cols);
+        this.outputRecords(res);
     }
 }
