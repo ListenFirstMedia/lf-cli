ff9a43711f761a56f483f1c03a19cee73805da74
Gregory Travis <greg.travis@listenfirstmedia.com>
2021-11-24 10:55:01 -0500

    cleanup

--- src/commands/bulk-tag-ingest/get.ts
+++ src/commands/bulk-tag-ingest/get.ts
@@ -20,24 +20,6 @@ export default class BulkTagIngestGet extends ApiCommand {
     static examples = ['$ lf-cli bulk-tag-ingest:get tags.csv'];
 
     async run() {
-    /* GET with filename
-        const opts = this.parse(BulkTagIngestGet);
-
-        if (opts.args.ID === 'help') {
-            await BulkTagIngestGet.run(['-h']);
-            this.exit(0);
-        }
-
-        const filename = opts.args.filename;
-
-        const res = await this.fetch(
-            `/v20200626/tag?job_id=5678&filename=${filename}`,
-            undefined,
-            `Ingesting tags from ${filename}'`
-        );
-
-        this.outputRecords(res);
-        */
 
         const opts = this.parse(BulkTagIngestGet);
 
@@ -46,35 +28,18 @@ export default class BulkTagIngestGet extends ApiCommand {
             this.exit(0);
         }
 
-        //const csv_contents = fs.readFileSync('tags.csv','utf8');
-        //console.log('tags.csv contents', csv_contents);
-        const jsonified = await csv(/*{
-            output: "csv"
-            }*/)
-        //.fromString(csv_contents)
+        const jsonified = await csv()
         .fromFile('tags.csv')
         .then(function(result){
-          console.log('csv result', result);
-          //const jsonified = result;
-          console.log('jsonified', result);
           return result;
         });
-        //console.flush();
-        //process.exit();
 
         const authData = JSON.stringify(jsonified);
 
-        /*
-        const authData = JSON.stringify({
-        job_id: 5678,
-        filename: 'input.csv'
-        });
-        */
         console.log('authData json', authData);
-        //const authData = "5678,input.csv\n";
 
         // prepare the POST request
-        const authReqOpts = {
+        const reqOpts = {
             method: 'POST',
             body: authData,
             headers: {
@@ -85,7 +50,7 @@ export default class BulkTagIngestGet extends ApiCommand {
 
         const res = await this.fetch(
             `/v20200626/tag`,
-            authReqOpts,
+            reqOpts,
             `Ingesting tags`
         );
 
