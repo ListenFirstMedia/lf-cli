7deccfb29b3ae780de2f8ddd723c12580db94580
Khrisendat Persaud <khris.persaud@listenfirstmedia.com>
2021-12-14 18:57:11 +0000

    WIP

--- src/commands/analytics/fetch-job-list.ts
+++ src/commands/analytics/fetch-job-list.ts
@@ -1,16 +1,31 @@
 import ApiCommand from '../../api-command';
+import { flags } from '@oclif/command';
 
 export default class FetchJobList extends ApiCommand {
     static description = `Return fetch jobs subitted by the user.`;
 
     static flags = {
         ...ApiCommand.flags,
+        'schedule-config-id': flags.integer({
+            description:
+                'Can be used to filter fetch job list by a particular schedule config.',
+        }),
     };
 
-    static examples = ['$ lf-cli analytics:fetch-job-list'];
+    static examples = [
+        '$ lf-cli analytics:fetch-job-list',
+        '$ lf-cli analytics:fetch-job-list --schedule-config-id 10',
+    ];
 
     async run() {
-        const path = `/v20200626/analytics/fetch_job`;
+        const opts = this.parse(FetchJobList);
+
+        let path = `/v20200626/analytics/fetch_job`;
+
+        if (opts.flags.schedule_config_id) {
+            path = `${path}?schedule_config_id=${opts.flags.schedule_config_id}`;
+        }
+
         const res = await this.fetch(path, undefined, `fetching`);
         let cols = {};
         cols = {
