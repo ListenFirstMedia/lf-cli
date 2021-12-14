6838999b0b6934a1a2fb51cd65e311baa288bf20
Mike Stanley <mike.stanley@listenfirstmedia.com>
2021-12-14 15:46:20 -0500

    fixed compile of lint staged, added compile job to ci, and yaml format support to prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,12 @@ module.exports = {
     singleQuote: true,
     printWidth: 80,
     tabWidth: 4,
+    overrides: [
+        {
+            files: ['*.yaml', '*.yml'],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
 };
