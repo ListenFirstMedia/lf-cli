699c69e0430be3334a8c345a737e38bfc38f4697
Miles Frankel <miles.frankel@gmail.com>
2021-12-14 13:56:13 -0500

    debug tests

--- src/lfapi/client.ts
+++ src/lfapi/client.ts
@@ -102,7 +102,7 @@ export default class Client {
         const fqUrl = await this.getApiUrl(relPath, this.#profile.api_host);
         const res = await _fetch(fqUrl.toString(), fetchOpts as any);
 
-        console.log(res.text());
+        console.log(await res.text());
 
         const data = await res.json();
         if (res.ok) {
