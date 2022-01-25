aaeb637c91ad58fd26fd35dde3f9c2e77e008f26
Khrisendat Persaud <khris.persaud@listenfirstmedia.com>
2022-01-25 06:16:57 +0000

    added schedule config

---
+++ src/commands/analytics/schedule-config-show.ts
@@ -0,0 +1,37 @@
+import ApiCommand from '../../api-command';
+
+export default class ScheduleConfigShow extends ApiCommand {
+    static description = `Return a schedule config subitted by the user.`;
+
+    static flags = {
+        ...ApiCommand.flags,
+    };
+
+    static args = [
+        {
+            name: 'ID',
+            description: 'the ID of the schedule config to retrieve',
+            required: true,
+        },
+    ];
+
+    static examples = ['$ lf-cli analytics:schedule-config-show 42'];
+
+    async run() {
+        const opts = this.parse(ScheduleConfigShow);
+
+        if (!opts.args.ID.match(/^[\d]+$/i)) {
+            this.error('Invalid Schedule Config ID', { exit: 1 });
+        }
+
+        const path = `/v20200626/analytics/schedule_config/${opts.args.ID}`;
+        const res = await this.fetch(path, undefined, `fetching`);
+        let cols = {};
+        cols = {
+            id: {},
+            state: {},
+        };
+
+        this.outputRecords(res, cols);
+    }
+}
